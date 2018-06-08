8ce865316b5b11c25a62ae0f9e474197723e8af9
Carlo da Veiga Monteiro <c.daveiga@live.nl>
2018-06-08 11:41:45 +0200

    * Shader creation * Triangle rendering

--- app/src/main/java/nl/carlodvm/androidapp/MainActivity.java
+++ app/src/main/java/nl/carlodvm/androidapp/MainActivity.java
@@ -1,12 +1,48 @@
 package nl.carlodvm.androidapp;
 
+import android.opengl.GLES20;
 import android.opengl.GLSurfaceView;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private GLSurfaceView mGLView;
+
+    private static int CompileShader(String source, int type){
+        int id = GLES20.glCreateShader(type);
+        GLES20.glShaderSource(id, source);
+        GLES20.glCompileShader(id);
+
+        int result[] = new int[1];
+        GLES20.glGetShaderiv(id, GLES20.GL_COMPILE_STATUS, result, 0);
+        if(result[0] == GLES20.GL_FALSE){
+            //TODO Implement all cases
+            System.out.println("Failed to compile " +
+                    (type == GLES20.GL_VERTEX_SHADER ? "vertex" :
+                    type == GLES20.GL_FRAGMENT_SHADER ? "fragment" : "")
+                    + " shader!\n" + GLES20.glGetShaderInfoLog(id));
+        }
+
+
+
+        return id;
+    }
+
+    static int CreateShader(String vertexShader, String fragmentShader){
+        int program = GLES20.glCreateProgram();
+        int vs = CompileShader(vertexShader, GLES20.GL_VERTEX_SHADER);
+        int fs = CompileShader(fragmentShader, GLES20.GL_FRAGMENT_SHADER);
+
+        GLES20.glAttachShader(program, vs);
+        GLES20.glAttachShader(program, fs);
+        GLES20.glLinkProgram(program);
+        GLES20.glValidateProgram(program);
+
+        GLES20.glDeleteShader(vs);
+        GLES20.glDeleteShader(fs);
+
+        return program;
+    }
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
--- app/src/main/java/nl/carlodvm/androidapp/MyGLRenderer.java
+++ app/src/main/java/nl/carlodvm/androidapp/MyGLRenderer.java
@@ -6,14 +6,45 @@ import android.opengl.GLSurfaceView;
 import java.nio.FloatBuffer;
 
 import javax.microedition.khronos.egl.EGLConfig;
-import javax.microedition.khronos.opengles.GL;
 import javax.microedition.khronos.opengles.GL10;
 
 public class MyGLRenderer implements GLSurfaceView.Renderer {
 
     @Override
     public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-        gl.glClearColor(1,0,0,0);
+        gl.glClearColor(0.4f,0,0.4f,0);
+        int buffers[] = new int[1];
+
+        float positions[] = {
+                -0.5f, -0.5f,
+                 0.0f,  0.5f,
+                 0.5f, -0.5f
+        };
+
+        GLES20.glGenBuffers(1, buffers, 0);
+        GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, buffers[0]);
+        GLES20.glBufferData(GLES20.GL_ARRAY_BUFFER, 6 * 4, FloatBuffer.wrap(positions), GLES20.GL_STATIC_DRAW);
+
+        GLES20.glEnableVertexAttribArray(0);
+        GLES20.glVertexAttribPointer(0, 2, GLES20.GL_FLOAT, false, 8, 0);
+
+        String vertexShader = "#version 320 es\n" +
+                "layout(location = 0) in vec4 position;" +
+                "\n" +
+                "void main(){" +
+                "gl_Position = position;" +
+                "}";
+
+        String fragmentShader = "#version 320 es\n" +
+                "precision mediump float;" +
+                "layout(location = 0) out vec4 color;" +
+                "\n" +
+                "void main(){" +
+                "color = vec4(1.0, 0.0, 0.0, 1.0);" +
+                "}";
+
+        int shader = MainActivity.CreateShader(vertexShader, fragmentShader);
+        GLES20.glUseProgram(shader);
     }
 
     @Override
@@ -24,7 +55,7 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
     @Override
     public void onDrawFrame(GL10 gl) {
         GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
-
+        GLES20.glDrawArrays(GLES20.GL_TRIANGLES, 0, 3);
 
     }
 }
