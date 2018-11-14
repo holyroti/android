dd849e1ca387dfa739d7381110a2c7de10d80777
MrCtje <c.daveiga@live.nl>
2018-11-14 01:39:17 +0100

    Add sceneform support and example

--- app/src/main/java/nl/carlodvm/androidapp/MainActivity.java
+++ app/src/main/java/nl/carlodvm/androidapp/MainActivity.java
@@ -1,65 +1,86 @@
 package nl.carlodvm.androidapp;
 
-import android.opengl.GLES20;
-import android.opengl.GLSurfaceView;
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.Context;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.ar.core.Anchor;
+import com.google.ar.sceneform.AnchorNode;
+import com.google.ar.sceneform.rendering.ModelRenderable;
+import com.google.ar.sceneform.rendering.Renderable;
+import com.google.ar.sceneform.ux.ArFragment;
+import com.google.ar.sceneform.ux.TransformableNode;
 
 public class MainActivity extends AppCompatActivity {
-    private GLSurfaceView mGLView;
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final double MIN_OPENGL_VERSION = 3.0;
 
-    private static int CompileShader(String source, int type){
-        int id = GLES20.glCreateShader(type);
-        GLES20.glShaderSource(id, source);
-        GLES20.glCompileShader(id);
-
-        int result[] = new int[1];
-        GLES20.glGetShaderiv(id, GLES20.GL_COMPILE_STATUS, result, 0);
-        if(result[0] == GLES20.GL_FALSE){
-            //TODO Implement all cases
-            System.out.println("Failed to compile " +
-                    (type == GLES20.GL_VERTEX_SHADER ? "vertex" :
-                    type == GLES20.GL_FRAGMENT_SHADER ? "fragment" : "")
-                    + " shader!\n" + GLES20.glGetShaderInfoLog(id));
-        }
-
-
-
-        return id;
-    }
-
-    static int CreateShader(String vertexShader, String fragmentShader){
-        int program = GLES20.glCreateProgram();
-        int vs = CompileShader(vertexShader, GLES20.GL_VERTEX_SHADER);
-        int fs = CompileShader(fragmentShader, GLES20.GL_FRAGMENT_SHADER);
-
-        GLES20.glAttachShader(program, vs);
-        GLES20.glAttachShader(program, fs);
-        GLES20.glLinkProgram(program);
-        GLES20.glValidateProgram(program);
-
-        GLES20.glDeleteShader(vs);
-        GLES20.glDeleteShader(fs);
-
-        return program;
-    }
-    // Used to load the 'native-lib' library on application startup.
-    static {
-        System.loadLibrary("native-lib");
-    }
+    private ArFragment arFragment;
+    private Renderable andyRenderable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
-        mGLView = new MyGLSurfaceView(this);
-        setContentView(mGLView);
-        // Example of a call to a native method
+
+        if (!checkIsSupportedDeviceOrFinish(this))
+            return;
+
+        setContentView(R.layout.activity_ux);
+        arFragment = (ArFragment) getSupportFragmentManager().findFragmentById(R.id.ux_fragment);
+
+        ModelRenderable.builder()
+                // To load as an asset from the 'assets' folder ('src/main/assets/andy.sfb'):
+                .setSource(this, Uri.parse("andy.sfb"))
+
+                // Instead, load as a resource from the 'res/raw' folder ('src/main/res/raw/andy.sfb'):
+                //.setSource(this, R.raw.andy)
+
+                .build()
+                .thenAccept(renderable -> andyRenderable = renderable)
+                .exceptionally(
+                        throwable -> {
+                            Log.e(TAG, "Unable to load Renderable.", throwable);
+                            return null;
+                        });
+
+        arFragment.setOnTapArPlaneListener(((hitResult, plane, motionEvent) -> {
+            if (andyRenderable == null)
+                return;
+
+            Anchor anchor = hitResult.createAnchor();
+            AnchorNode anchorNode = new AnchorNode(anchor);
+            anchorNode.setParent(arFragment.getArSceneView().getScene());
+
+            TransformableNode andy = new TransformableNode(arFragment.getTransformationSystem());
+            andy.getScaleController().setMinScale(0.1f);
+            andy.setParent(anchorNode);
+            andy.setRenderable(andyRenderable);
+            andy.select();
+        }));
     }
 
-    /**
-     * A native method that is implemented by the 'native-lib' native library,
-     * which is packaged with this application.
-     */
-    public native String stringFromJNI();
+    public static boolean checkIsSupportedDeviceOrFinish(final Activity activity) {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+            Log.e(TAG, "Sceneform requires Android N or later");
+            Toast.makeText(activity, "Sceneform requires Android N or later", Toast.LENGTH_LONG).show();
+            activity.finish();
+            return false;
+        }
+        String openGLVersionString = ((ActivityManager) activity.getSystemService(Context.ACTIVITY_SERVICE))
+                .getDeviceConfigurationInfo()
+                .getGlEsVersion();
+        if (Double.parseDouble(openGLVersionString) < MIN_OPENGL_VERSION) {
+            Log.e(TAG, "Sceneform requires OpenGL ES 3.0 or later");
+            Toast.makeText(activity, "Sceneform requires OpenGL ES 3.0 or later", Toast.LENGTH_LONG).show();
+            activity.finish();
+            return false;
+        }
+        return true;
+    }
 }
